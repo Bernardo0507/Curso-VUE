5d7029a4e1b5761b6856bcdeb95c908b51bd8a2e
Bernardo <bernardo.sm10@gmail.com>
2022-05-24 19:26:41 -0300

    Terceiro-commit-eventos

---
+++ Dom/22_hora_de_praticar_template/app.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#desafio',
+    data: {
+        nome: 'Bernardo Santos Moreira',
+        idade: 28,
+        imagem: 'https://publisher-publish.s3.eu-central-1.amazonaws.com/pb-brasil247/swp/jtjeq9/media/20180706140720_5b3f5f5f79b75f796a679bb0jpeg.jpeg'
+    },     
+})
